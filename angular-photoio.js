85b0cec7d04bade90d8488ceb0c0b8940015752c
Joshua Powell <joshua@viable.io>
2015-06-10 17:36:59 -0400

    Added preliminary Directive for Photo.io

---
+++ angular-photoio.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('photoio')
+  .directive('photoio', function () {
+    return {
+      restrict: 'A',
+      replace: true,
+      template: '<img class="" src="{{ render }}" alt="" width="{{ photoioW }}" />',
+      scope: {
+        render: '=?',
+        photoioClass: '=?',
+        photoioU: '=?',
+        photoioW: '=?',
+        photoioH: '=?',
+        photoioMode: '=?',
+        photoioQuality: '=?'
+      },
+      link: function(scope, element, attrs) {
+      
+        scope.photoioMode = (scope.photoioMode) ? scope.photoioMode : 'crop';
+
+        scope.render = 'http://p.hoto.io/img?u=' + encodeURI(scope.photoioU) + '&w=' + scope.photoioW + '&w=' + scope.photoioH + '&w=' + scope.photoioMode;
+ 
+      }
+    };
+  });
